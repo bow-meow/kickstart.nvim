1645ce9285d996b5d922ab9cf0ede4abf34c3122
bow-meow <33474324+bow-meow@users.noreply.github.com>
2025-02-07 06:45:47 +0000

    update harpoon to harpoon2

--- lua/bowmeow/plug/harpoon.lua
+++ lua/bowmeow/plug/harpoon.lua
@@ -1,6 +1,8 @@
 local local_plugins = {
     {
         "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local harpoon = require("harpoon")
 
