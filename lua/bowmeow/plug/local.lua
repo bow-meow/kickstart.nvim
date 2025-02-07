764559189002a216a0d52937f767890fd84de907
bow-meow <33474324+bow-meow@users.noreply.github.com>
2025-02-07 05:27:20 +0000

    fix local

--- lua/bowmeow/plug/local.lua
+++ lua/bowmeow/plug/local.lua
@@ -1,33 +1,7 @@
-
 local local_plugins = {
     {
-        "cockpit",
-        dir = "~/personal/cockpit",
-        config = function()
-            require("cockpit")
-            vim.keymap.set("n", "<leader>ct", "<cmd>CockpitTest<CR>")
-            vim.keymap.set("n", "<leader>cr", "<cmd>CockpitRefresh<CR>")
-        end,
-    },
-
-    {
-        "streamer",
-        dir = "~/personal/eleven-streamer",
-        dependencies = {
-            "livinglogic-nl/ws.nvim"
-        },
-        config = function() end
-    },
-
-
-    {
-        "caleb",
-        dir = "~/personal/caleb",
-        config = function() end
-    },
-    {
         "harpoon",
-        dir = "~/personal/harpoon",
+        dir = "ThePrimeagen/harpoon",
         config = function()
             local harpoon = require("harpoon")
 
@@ -46,22 +20,7 @@ local local_plugins = {
             vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
             vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
         end
-    },
-    {
-        "vim-apm", dir = "~/personal/vim-apm",
-        config = function()
-            --[[
-            local apm = require("vim-apm")
-
-            apm:setup({})
-            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-            --]]
-        end
-    },
-    {
-        "vim-with-me", dir = "~/personal/vim-with-me",
-        config = function() end
-    },
+    }
 }
 
 return local_plugins
